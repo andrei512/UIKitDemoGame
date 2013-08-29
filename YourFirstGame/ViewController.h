813faf57eb062f1fdeaeacef8d73b0c18e98a447
Andrei Puni <puni.andrei23@gmail.com>
2013-08-30 01:35:00 +0300

    Initial Commit

---
+++ YourFirstGame/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  YourFirstGame
+//
+//  Created by Andrei on 8/30/13.
+//  Copyright (c) 2013 Andrei Puni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ YourFirstGame/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  YourFirstGame
+//
+//  Created by Andrei on 8/30/13.
+//  Copyright (c) 2013 Andrei Puni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
